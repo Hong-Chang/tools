7f0f41e5a15cd585fb72dd9cd77170becef1d382
Hong-Chang <62316052+Hong-Chang@users.noreply.github.com>
2021-02-04 13:25:23 -0800

    Update setup-machine-arktos.sh

--- setup-machine-arktos.sh
+++ setup-machine-arktos.sh
@@ -77,27 +77,8 @@ git tag v1.15.0
 
 echo Setup: Install Docker
 
-sudo apt-get update -y -q
-
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common -y -q
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
-sudo apt-get update -y -q
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y -q
+sudo apt -y install docker.io
 sudo gpasswd -a $USER docker
-
 
 ####################
 
