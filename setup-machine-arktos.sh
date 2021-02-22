ffec9961f0a3597c729236d8a9f81912081a4a86
Hong-Chang <62316052+Hong-Chang@users.noreply.github.com>
2021-02-22 11:13:58 -0800

    Update setup-machine-arktos.sh

--- setup-machine-arktos.sh
+++ setup-machine-arktos.sh
@@ -21,7 +21,7 @@
 
 ####################
 
-# echo Setup: Install kopf
+echo Setup: Install kopf
 
 sudo apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 wget -O /home/ubuntu/Python-3.8.8.tgz https://www.python.org/ftp/python/3.8.8/Python-3.8.8.tgz
